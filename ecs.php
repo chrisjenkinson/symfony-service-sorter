dd8a947c79436d77d8ef3aa6971b8aab9edcee97
Chris Jenkinson <chris@chrisjenkinson.org>
2026-04-13 22:27:19 +1200

    feat: initial project scaffold

---
+++ ecs.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/bin',
+    ])
+    ->withPreparedSets(psr12: true);
