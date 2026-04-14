c56b13f9c9056436013cf5d9d545ce1edbf6d961
Chris <chris@chrisjenkinson.org>
2026-04-14 20:24:05 +1200

    feat: add SortServicesCommand and bin/sort-services entry point (#4)
    Wires parser and sorter into a Symfony Console command.
    Outputs to stdout by default; --write overwrites the file in-place.
    Exits 1 on file-not-found or write failure.

---
+++ src/Command/SortServicesCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Parser\YamlServiceParser;
+use App\Sorter\ServicesSorter;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'sort-services',
+    description: 'Sorts entries in the services: key of a Symfony YAML file alphabetically',
+)]
+final class SortServicesCommand extends Command
+{
+    public function __construct(
+        private readonly YamlServiceParser $parser,
+        private readonly ServicesSorter $sorter,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('file', InputArgument::REQUIRED, 'Path to the YAML file')
+            ->addOption('write', 'w', InputOption::VALUE_NONE, 'Write sorted output back to the file instead of stdout');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $errorOutput = $output instanceof ConsoleOutputInterface
+            ? $output->getErrorOutput()
+            : $output;
+
+        /** @var string $filePath */
+        $filePath = $input->getArgument('file');
+
+        if (!file_exists($filePath)) {
+            $errorOutput->writeln(sprintf('<error>File not found: %s</error>', $filePath));
+            return Command::FAILURE;
+        }
+
+        $content = file_get_contents($filePath);
+
+        if ($content === false) {
+            $errorOutput->writeln(sprintf('<error>Could not read file: %s</error>', $filePath));
+            return Command::FAILURE;
+        }
+
+        $parsedFile = $this->parser->parse($content);
+
+        if ($parsedFile->servicesHeader === '') {
+            $errorOutput->writeln(sprintf(
+                '<comment>Warning: no services: key found in %s</comment>',
+                $filePath,
+            ));
+        }
+
+        $sorted = $this->sorter->sort($parsedFile);
+
+        if ($input->getOption('write')) {
+            $written = file_put_contents($filePath, $sorted);
+            if ($written === false) {
+                $errorOutput->writeln(sprintf('<error>Could not write to file: %s</error>', $filePath));
+                return Command::FAILURE;
+            }
+            return Command::SUCCESS;
+        }
+
+        $output->write($sorted, false, OutputInterface::OUTPUT_RAW);
+
+        return Command::SUCCESS;
+    }
+}
